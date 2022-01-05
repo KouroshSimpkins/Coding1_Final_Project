8ef50c2cb2b43e048f7d00be8028744e3b81109d
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2022-01-05 22:01:40 +0000

    test file, to store changes

---
+++ testing_stuff.py
@@ -0,0 +1,6 @@
+"""A module for testing functions in a sandbox."""
+
+import numpy as np
+arr = np.array([[1,2,3],[4,5,6]])
+array = [["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"], ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"]]
+print(array.join("\n"))
