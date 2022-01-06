635ce3536a4d0f1f7461491d84c567d2c08ce075
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2022-01-06 22:53:35 +0000

    Functional Stuff

--- testing_stuff.py
+++ testing_stuff.py
@@ -10,19 +10,22 @@ ships = {"Aircraft Carrier": 5,
          "Patrol Boat": 2}
 
 board = []
-for i in range(10):
-    board_row = []
-    for j in range(10):
-        board_row.append(-1)
-    board.append(board_row)
 
-player1_board = copy.deepcopy(board), "player1"
-player2_board = copy.deepcopy(board), "player2"
 
-print(ships)
-print(player1_board)
-print(player2_board)
+def setup_board():
+    for i in range(10):
+        board_row = []
+        for j in range(10):
+            board_row.append(-1)
+        board.append(board_row)
 
+    player1_board = copy.deepcopy(board), "player1"
+    player2_board = copy.deepcopy(board), "player2"
+
+    return player1_board, player2_board
+
+
+player1_board, player2_board = setup_board()
 
 def print_board(board):
     for i in board[0]:
