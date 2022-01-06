64d0e5fd879fb455b790cb9ce058bbf6340d9867
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2022-01-06 00:00:57 +0000

    Battleship stuff

--- testing_stuff.py
+++ testing_stuff.py
@@ -3,4 +3,4 @@
 import numpy as np
 arr = np.array([[1,2,3],[4,5,6]])
 array = [["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"], ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"]]
-print(array.join("\n"))
+print(array)
