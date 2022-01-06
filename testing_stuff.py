dbb9343e679f6682c7ac4cb500ef05decca9b2a7
Kourosh Simpkins <kouroshsimpkins@gmail.com>
2022-01-06 11:33:13 +0000

    working on board

--- testing_stuff.py
+++ testing_stuff.py
@@ -1,6 +1,33 @@
 """A module for testing functions in a sandbox."""
 
-import numpy as np
-arr = np.array([[1,2,3],[4,5,6]])
-array = [["[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]"], ["[ ]", "[ ]", "[ ]", "[ ]", "[ ]"]]
-print(array)
+import copy
+
+
+ships = {"Aircraft Carrier": 5,
+         "Battleship": 4,
+         "Submarine": 3,
+         "Destroyer": 3,
+         "Patrol Boat": 2}
+
+board = []
+for i in range(10):
+    board_row = []
+    for j in range(10):
+        board_row.append(-1)
+    board.append(board_row)
+
+player1_board = copy.deepcopy(board), "player1"
+player2_board = copy.deepcopy(board), "player2"
+
+print(ships)
+print(player1_board)
+print(player2_board)
+
+
+def print_board(board):
+    for i in board[0]:
+        print(i)
+    print(board[1])
+
+print_board(player1_board)
+print_board(player2_board)
